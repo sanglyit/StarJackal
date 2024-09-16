6c81e4c8193794e513dc262ccf36e7c9a72085d7
Maga7w7 <99457132+imKhoa1215@users.noreply.github.com>
2024-09-16 09:21:23 +0700

    V7.1 (Inventory Update)
    Completed Inventory UI tutorial in youtube

--- Assets/GameScript/Player/InventoryManager.cs
+++ Assets/GameScript/Player/InventoryManager.cs
@@ -1,24 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryManager : MonoBehaviour
 {
     public List<GunController> weaponSlots = new List<GunController>(6);
     public int[] weaponLevels = new int[6];
+    public List<Image> weaponUISlots = new List<Image>(6);
+
     public List<PassiveItem> passiveItemSlots = new List<PassiveItem>(6);
     public int[] passiveItemLevels = new int[6];
+    public List<Image> passiveItemUISlots = new List<Image>(6);
 
     public void AddWeapon(int slotIndex, GunController weapon) //Add a weapon to a specific slot
     {
         weaponSlots[slotIndex] = weapon;
         weaponLevels[slotIndex] = weapon.weaponData.Level;
+        weaponUISlots[slotIndex].enabled = true; //Enable the image component
+        weaponUISlots[slotIndex].sprite = weapon.weaponData.Icon;
     }
 
     public void AddPassiveItem(int slotIndex, PassiveItem passiveItem) //Add a passive item to a specific slot
     {
         passiveItemSlots[slotIndex] = passiveItem;
         passiveItemLevels[slotIndex] = passiveItem.passiveItemData.Level;
+        passiveItemUISlots[slotIndex].enabled = true; //Enable the image component
+        passiveItemUISlots[slotIndex].sprite = passiveItem.passiveItemData.Icon;
     }
 
     public void LevelUpWeapon(int slotIndex)
